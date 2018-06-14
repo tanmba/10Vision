d7efaaac61aa48a0daa1b9fa3f00ce54137d26fd
tanmba <tan-mba.takougnadi@hetic.net>
2018-06-14 15:25:42 +0200

    Fist commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD6_990Df5mC2M-wI8N2VX8TA1Zaw0iiyc",
+    authDomain: "vision-ffda9.firebaseapp.com",
+    databaseURL: "https://vision-ffda9.firebaseio.com",
+    projectId: "vision-ffda9",
+    storageBucket: "vision-ffda9.appspot.com",
+    messagingSenderId: "506458494999"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD6_990Df5mC2M-wI8N2VX8TA1Zaw0iiyc",
+    authDomain: "vision-ffda9.firebaseapp.com",
+    databaseURL: "https://vision-ffda9.firebaseio.com",
+    projectId: "vision-ffda9",
+    storageBucket: "vision-ffda9.appspot.com",
+    messagingSenderId: "506458494999"
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
