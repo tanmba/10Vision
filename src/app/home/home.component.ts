907dc6aaf4ef7dab3dd1ec481f97ef3eb2b95cb7
tanmba <tan-mba.takougnadi@hetic.net>
2018-06-28 16:33:14 +0200

    Update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  availableText: false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  notAvailable() {
+    this.availableText = !this.availableText;
+  }
 }
