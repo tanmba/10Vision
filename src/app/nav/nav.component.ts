1d500e22976a89fbd6520f15fc39e0b4161cd731
tanmba <tan-mba.takougnadi@hetic.net>
2018-06-29 01:11:11 +0200

    update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  homeActive = false;
+  gabiActive = false;
+  profileActive = false;
+  plusActive = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  homeSelected() {
+    this.homeActive = !this.homeActive;
+    this.gabiActive = false;
+    this.profileActive = false;
+    this.plusActive = false;
+    this.router.navigate(['/users']);
+  }
+
+  gabiSelected() {
+    this.gabiActive = !this.gabiActive;
+    this.profileActive = false;
+    this.plusActive = false;
+    this.homeActive = false;
+  }
+
+  profileSelected() {
+    this.profileActive = !this.profileActive;
+    this.plusActive = false;
+    this.homeActive = false;
+    this.gabiActive = false;
+    this.router.navigate(['/user']);
+  }
+
+  plusSelected() {
+    this.plusActive = !this.plusActive;
+    this.homeActive = false;
+    this.gabiActive = false;
+    this.profileActive = false;
+  }
 }
